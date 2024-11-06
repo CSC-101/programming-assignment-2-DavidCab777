0aa5518ac19fea7a2e4a504c37c5c14d6eb91bb2
David Cabrera <dcabre12@calpoly.edu>
2024-11-06 01:40:43 -0800

    m

--- hw2.py
+++ hw2.py
@@ -3,18 +3,72 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def create_rectangle(nums1: data.Point, nums2:data.Point):
+    highest_y = max(nums1.y, nums2.y)  #looks for top left point
+    left_most_x = min(nums1.x, nums2.x)
+    right_most_x = max(nums1.x, nums2.x)  #looks for bottom right
+    lowest_y = min(nums1.y, nums2.y)
+    top_left = data.Point(left_most_x,highest_y)  #creates top left
+    bottom_right = data.Point(right_most_x, lowest_y) # creates bottom right
+    rectangle = (top_left, bottom_right)
+    return rectangle
 
 # Part 2
-
-
+def shorter_duration_time(input1:data.Duration, input2:data.Duration):
+    total_seconds1 = input1.minutes * 60 + input1.seconds
+    total_seconds2 = input2.minutes * 60 + input2.seconds
+    if total_seconds1 > total_seconds2:
+        return True
+    else:
+        return False
 # Part 3
-
-
+def song_short_than(lst: list[data.Song], tim: data.Duration):
+    short_songs = []
+    max_time = tim.minutes * 60 + tim.seconds
+    for data.Song in lst:
+        song_duration_seconds = data.Song.duration.minutes * 60 + data.Song.duration.seconds
+        if song_duration_seconds < max_time: short_songs.append(data.Song)
+    return short_songs
 # Part 4
-
+def running_time(lst: list[data.Song], lst2: list[int]):
+    seconds = 0
+    minutes = 0
+    for idx in lst2:
+        if 0 <= idx < len(lst):
+            minutes += lst[idx].duration.minutes
+            seconds += lst[idx].duration.seconds
+    minutes += seconds // 60
+    seconds = seconds % 60
+    total_time = data.Duration(minutes, seconds)
+    return total_time
 
 # Part 5
+def validate_route(lst:list[list[str]], lst2:list[str]):
+    if not lst2 or len(lst2) == 1:
+        return False
 
-
+    for idx in range(len(lst2)-1):
+        if [lst2[idx], lst2[idx + 1]] not in lst:
+            return False
+        else:
+            return True
 # Part 6
+def longest_repetition(lst:list[int]):
+    longest_index = None
+    longest_length = 0
+    current_length = 1
+    current_index = 0
+
+    for i in range(1, len(lst)):
+        if lst[i] == lst[i - 1]:
+            current_length += 1
+        else:
+            if current_length > longest_length:
+                longest_length = current_length
+                longest_index = current_index
+            current_length = 1
+            current_index = i
+    if current_length > longest_length:
+        longest_index = current_index
+
+    return longest_index
--- hw2_tests.py
+++ hw2_tests.py
@@ -7,21 +7,91 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_create_rectangle1(self):
+        input1 = data.Point(2,4)
+        input2 = data.Point(19,1)
+        result = hw2.create_rectangle(input1,input2)
+        expected = (data.Point(2, 4), data.Point(19,1))
+        self.assertEqual(result, expected)
 
-
+    def test_create_rectangle2(self):
+        input1 = data.Point(10,20)
+        input2 = data.Point(1,2)
+        result = hw2.create_rectangle(input1,input2)
+        expected = (data.Point(1, 20), data.Point(10,2))
+        self.assertEqual(result, expected)
     # Part 2
+    def test_shorter_duration_time1(self):
+        input1 = data.Duration(12,23)
+        input2 = data.Duration(13,24)
+        result = hw2.shorter_duration_time(input1,input2)
+        expected = False
+        self.assertEqual(result, expected)
 
+    def test_shorter_duration_time2(self):
+        input1 = data.Duration(20,23)
+        input2 = data.Duration(13,24)
+        result = hw2.shorter_duration_time(input1,input2)
+        expected = True
+        self.assertEqual(result, expected)
 
     # Part 3
+    def test_song_shorter_than1(self):
+        lst = [data.Song("Song 1", "Title 1", data.Duration(2,3)), data.Song("Song 2", "Title 2", data.Duration(4,4))]
+        dur = data.Duration(3, 0)
+        result = hw2.song_short_than(lst,dur)
+        expected = [data.Song("Song 1", "Title 1", data.Duration(2,3))]
+        self.assertEqual(result, expected)
 
+    def test_song_shorter_than2(self):
+        lst = [data.Song("Song 1", "Title 1", data.Duration(2,31)), data.Song("Song 2", "Title 2", data.Duration(2,29))]
+        dur = data.Duration(2, 30)
+        result = hw2.song_short_than(lst,dur)
+        expected = [data.Song("Song 2", "Title 2", data.Duration(2,29))]
+        self.assertEqual(result, expected)
 
     # Part 4
+    def test_running_time1(self):
+        lst = [data.Song("Song 1", "Title 1", data.Duration(2,3)), data.Song("Song 2", "Title 2", data.Duration(4,4))]
+        lst2 = [0,1]
+        result = hw2.running_time(lst, lst2)
+        expected = data.Duration(6,7)
+        self.assertEqual(result, expected)
 
-
+    def test_running_time2(self):
+        lst = [data.Song("Song 1", "Title 1", data.Duration(2,3)), data.Song("Song 2", "Title 2", data.Duration(4,4))]
+        lst2 = [0,1,0]
+        result = hw2.running_time(lst, lst2)
+        expected = data.Duration(8,10)
+        self.assertEqual(result, expected)
     # Part 5
+    def test_validate_route1(self):
+        city_links = [['san luis obispo', 'santa margarita'], ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],['atascadero', 'creston']]
+        route = ['san luis obispo', 'santa margarita', 'atascadero']
+        result = hw2.validate_route(city_links, route)
+        expected = True
+        self.assertEqual(result, expected)
 
-
+    def test_validate_route2(self):
+        city_links = [['san luis obispo', 'santa margarita'], ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],['atascadero', 'creston']]
+        route = ['san luis obispo', 'atascadero']
+        result = hw2.validate_route(city_links, route)
+        expected = False
+        self.assertEqual(result, expected)
     # Part 6
+    def test_longest_repetition1(self):
+        lst = [1,1,2,3,4,2,3,3,5,3,10,3,3]
+        result = hw2.longest_repetition(lst)
+        expected = 0
+        self.assertEqual(result, expected)
+
+    def test_longest_repetition2(self):
+        lst = [1,1,1,2,2,2,2,3,4,1,1,5]
+        result = hw2.longest_repetition(lst)
+        expected = 3
+        self.assertEqual(result, expected)
 
 
 
